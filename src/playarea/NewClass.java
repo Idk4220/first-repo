74ec61f3bf07d3319e76ff67f92ac72e885a9084
Isaac Donkor 4221 <idonkor028@gmail.com>
2022-11-03 13:47:24 +0000

    First Commit

---
+++ src/playarea/NewClass.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package playarea;
+
+/**
+ *
+ * @author 23350
+ */
+public class NewClass {
+    double length;
+    double height;
+   double base;
+   double hypotenus;
+    public void CalPerimeter(){
+    double perimeter;
+    perimeter=(length)+(base)+(hypotenus);
+    System.out.println("the perimeter is "+perimeter);
+    }
+ public void CalArea(double length, double height){
+ double area;
+ area=(base*height)*1/2;
+ System.out.println("the area is "+area);
+     
+ }
+ 
+
+ 
+ }
---
+++ src/playarea/PlayArea.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package playarea;
+
+/**
+ *
+ * @author 23350
+ */
+public class PlayArea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    NewClass triangle=new NewClass();
+    triangle.height=15;
+    triangle.length=20;
+    triangle.base= 15;
+    triangle.hypotenus=10;
+    System.out.println(triangle.height);
+    System.out.println(triangle.length);
+ triangle.CalPerimeter();
+triangle.CalArea(15.0, 15.0);
+    
+    }
+    
+    
+}
